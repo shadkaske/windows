098c1fe0d16f3255fc6d2088b43487a82143d58f
Shad Kaske <shadkaske@gmail.com>
2026-03-15 11:48:14 -0500

    Added help to main script and flags to do different things

--- system_config.ps1
+++ system_config.ps1
@@ -1,21 +1,35 @@
 param (
-    [switch]$PackagesOnly,
-    [switch]$FontsOnly
+    [switch]$Packages,
+    [switch]$Fonts,
+    [switch]$Configs,
+    [switch]$Help
 )
 
+if ($Help -or ($Packages -eq $false -and $Fonts -eq $false -and $Configs -eq $false)) {
+    Write-Host "Usage: .\system_config.ps1 [options]"
+    Write-Host "Options:"
+    Write-Host "  -Packages    Install packages and PowerShell modules"
+    Write-Host "  -Fonts       Install Nerd Fonts"
+    Write-Host "  -Configs     Create symbolic links for config files"
+    Write-Host "  -Help        Show this help message"
+    Write-Host "Examples:"
+    Write-Host "  .\system_config.ps1 -Packages -Fonts  # Install packages and fonts"
+    Write-Host "  .\system_config.ps1 -Configs          # Link configs"
+    exit
+}
+
 # Install packages if requested
-if ($PackagesOnly -or (!$FontsOnly)) {
+if ($Packages) {
     . .\packages.ps1
 }
 
 # Install fonts if requested
-if ($FontsOnly -or (!$PackagesOnly)) {
+if ($Fonts) {
     . .\fonts.ps1
 }
 
-# Ask user if they want to link configs
-$linkConfigs = Read-Host "Do you want to link configs? (y/n)"
-if ($linkConfigs -eq 'y' -or $linkConfigs -eq 'Y') {
+# Link configs if requested
+if ($Configs) {
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File $PWD\symlink_configs.ps1" -Verb RunAs -Wait
 }
 
