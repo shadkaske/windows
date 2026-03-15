e8c192b649c012ebc0903bfd79df03dcd90c6550
Shad Kaske <shadkaske@gmail.com>
2026-03-15 13:24:44 -0500

    Added tab complete for winget to powerwhell profile

--- configs/Documents/PowerShell/profile.ps1
+++ configs/Documents/PowerShell/profile.ps1
@@ -22,3 +22,13 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory
 Set-PSFzfOption -TabExpansion
 
 Invoke-Expression (&starship init powershell)
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
